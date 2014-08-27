35ce3eab07d8b0bea4db159038126b691bf42065
Renaud <renaud@venuenext.com>
2014-08-27 11:33:11 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   root 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
